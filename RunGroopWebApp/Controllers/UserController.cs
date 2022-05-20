4490aca125cc78369658346db71c1f677fb10a6f
Kostarkis <bartoszkostarczyk1@gmail.com>
2022-05-20 10:51:13 +0200

    Episode 18 - Displaying profiles and showing other runners to users

---
+++ RunGroopWebApp/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Dtos;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.Maps;
+
+namespace RunGroopWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserController(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+        [HttpGet("users")]
+        public async Task<IActionResult> Index()
+        {
+            var users =  await _userRepository.GetAllUsers();
+            List<IndexUserDto> result = new List<IndexUserDto>();
+            foreach (var user in users)
+            {
+                var userDto = _mapper.Map<IndexUserDto>(user);
+                result.Add(userDto);
+            }
+            return View(result);
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            var user = await _userRepository.GetUserById(id);
+            var userDto = _mapper.Map<DetailsUserDto>(user);
+            return View(userDto);
+        }
+    }
+}
---
+++ RunGroopWebApp/Maps/UserMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using RunGroopWebApp.Dtos;
+using RunGroopWebApp.Models;
+
+namespace RunGroopWebApp.Maps
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<User, IndexUserDto>()
+                /*.ForMember(m => m.UserName, c => c.MapFrom(a => a.UserName))
+                .ForMember(m => m.Id, c => c.MapFrom(c => c.Id))*/
+                //.ForMember(m => _phtService.AddPhotoAsync(m.ImageFile).Result.Url.ToString(), c => c.MapFrom(c => c.Image))
+                .ReverseMap();
+
+            CreateMap<User, DetailsUserDto>()
+                .ReverseMap();
+        }
+    }
+}
